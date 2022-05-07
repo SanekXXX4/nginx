b9dbb45619929d3f623a497b766dfd9fc2a8287d
SanekXXX4 <gridmp@mail.ru>
2022-05-07 11:40:26 +0400

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -2,9 +2,9 @@
 if [ -z "$1" ]
 then
 echo "Установка Nginx, PHP, phpmyadmin, MariaDB, Memchached"
-apt update
-apt install nginx
-apt-get install ufw
+apt update -y
+apt install nginx -y
+apt-get install ufw -y
 ufw allow 80/tcp
 ufw allow 443/tcp
 ufw allow 22/tcp
@@ -16,37 +16,39 @@ ufw allow 'Nginx Full'
 ufw reload
 sed -i 's/ENABLED=no/ENABLED=yes/' /etc/ufw/ufw.conf
 ufw reload
-apt install wget curl
+apt install wget curl -y
 wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
 chmod +x mariadb_repo_setup
  ./mariadb_repo_setup \
 
 --mariadb-server-version="mariadb-10.6"
-apt update
-apt install mariadb-server mariadb-backup mariadb-client
+apt update -y
+apt install mariadb-server mariadb-backup mariadb-client -y
 systemctl start mariadb
 systemctl enable mariadb
 mysql_secure_installation
-apt install php php-fpm php-common php-mysql php-gd php-cli
-apt purge apache2
-apt-get install software-properties-common
+apt install php php-fpm php-common php-mysql php-gd php-cli -y
+apt purge apache2 -y
+apt-get install software-properties-common -y
 add-apt-repository ppa:ondrej/php
-apt-get update
-apt install php8.0-common
-apt install php8.0-cli
-apt install php8.0-{curl,intl,mysql,readline,xml,mbstring}
-apt install php8.0-pcov # PCOV code coverage tool
-apt install php8.0-xdebug
-apt install php8.0-fpm
-apt-get install php8.0-memcache php8.0-memcached
-apt-get install memcached
-apt install phpmyadmin
+apt-get update -y
+apt install php8.0-common -y
+apt install php8.0-cli -y
+apt install php8.0-{curl,intl,mysql,readline,xml,mbstring} -y
+apt install php8.0-pcov # PCOV code coverage tool -y
+apt install php8.0-xdebug -y
+apt install php8.0-fpm -y
+apt-get install php8.0-memcache php8.0-memcached -y
+apt-get install memcached -y
+apt install phpmyadmin -y
 ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
 chmod 775 -R /usr/share/phpmyadmin/
 chown root:nginx -R /usr/share/phpmyadmin/
 read -p "Введите пароль учетной записи MySQL admin : " password
-echo "В оболочке MariaDB введите: CREATE USER 'admin'@'localhost' IDENTIFIED BY '$password';"
-echo "В оболочке MariaDB введите: GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;"
+echo "В оболочке MariaDB введите:"
+echo "CREATE USER 'admin'@'localhost' IDENTIFIED BY '$password';"
+echo "В оболочке MariaDB введите:"
+echo "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;"
 echo "В оболочке MariaDB введите: FLUSH PRIVILEGES;"
 mysql -u root -p
 ssl phpmyadmin
